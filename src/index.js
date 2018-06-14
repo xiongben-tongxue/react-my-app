12c894c35004b138730507fab6e8be7e342a8b0c
xiongben <wangdongkf@163.com>
2018-06-14 21:32:23 +0800

    修复第一行代码

--- src/index.js
+++ src/index.js
@@ -6,3 +6,8 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+);
